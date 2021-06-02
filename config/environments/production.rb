2842bfc07a3f182032b41336f0697abea1f0c1b5
Carrilho <rafael.carrilho@injunior.com.br>
2021-06-02 19:51:35 -0300

    novo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "chat_production"
 
   config.action_mailer.perform_caching = false
-
+  config.web_socket_server_url = "wss://chatin-vivo.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ["http://localhost:3000", "http://localhost:3001"]
+ 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
