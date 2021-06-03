6ec287e5588b1ed2584f7c813648c68325f4d050
Carrilho <rafael.carrilho@injunior.com.br>
2021-06-02 21:43:47 -0300

    p

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -2,6 +2,5 @@ class ConversationSerializer < ActiveModel::Serializer
   attributes :id
   belongs_to :speaker2, class_name: "User"
   belongs_to :speaker1, class_name: "User"
-  has_many :messages, order: 'created_at'
-
+  has_many :messages
 end
--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,10 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :content
+  attributes :id, :content, :name_sender, :id_sender
   belongs_to :conversation
-  belongs_to :sender
+  def name_sender
+    self.object.sender.name
+  end
+  def id_sender
+    self.object.sender.id
+  end
 end
